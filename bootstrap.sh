3987409e47913376614afc533a8398ff19d7f3d9
Lubomir Host <lubomir.host@gmail.com>
2016-10-23 23:11:54 +0200

    Added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+if [ ! -d .git ]; then
+	git clone https://github.com/lhost/scripts-git && cd scripts-git && ./bootstrap.sh
+fi
+
+cwd=`pwd`
+
+if [ ! -d "$HOME/bin" ]; then
+	mkdir $HOME/bin 
+fi
+
+# create symlink for every Perl script in repository
+cd $HOME/bin
+for i in $cwd/*.pl; do
+	ln -s $i
+done
